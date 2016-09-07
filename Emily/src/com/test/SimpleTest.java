136d7c731e60d2201331f5c07f5d8e7285587f2c
TongQianwen <941375265@qq.com>
2016-09-07 09:29:14 +0800

    可以运行一系列测试

--- Emily/src/com/test/SimpleTest.java
+++ Emily/src/com/test/SimpleTest.java
@@ -1,21 +1,48 @@
 package com.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Administrator on 2016/9/6.
  */
 public class SimpleTest {
+    private Collection<Object> collection;
+    @Before
+    public void setUp() {
+        collection = new ArrayList<>();
+    }
+
     @Test
     public void testEmptyCollection() {
-        Collection collection = new ArrayList();
         assertTrue(collection.isEmpty());
     }
+
+
+    @Test
+    public void testOneItemCollection() {
+        collection.add("itemA");
+        assertEquals(1, collection.size());
+    }
+
+    @Test
+    public void testNotEmptyCollection() {
+        assertFalse(collection.isEmpty());
+    }
+
+    @Test
+    public void testTwoItemCollection() {
+        collection.add("itemB");
+        assertEquals(1, collection.size());
+    }
+
     public static void main(String args[]) {
         org.junit.runner.JUnitCore.main("com.test.SimpleTest");
     }
